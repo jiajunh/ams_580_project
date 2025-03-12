f14e8d391994ca93305e374630a9589e9d2dcaf0
jiajunh <jiajunh@umich.edu>
2025-03-12 09:18:26 -0400

    mew

---
+++ eval.py
@@ -0,0 +1,22 @@
+import os
+import argparse
+import numpy as np
+import pandas as pd
+
+from utils import set_seed
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", default="./data/", type=str)
+    parser.add_argument("--seed", default=42, type=int)
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+    set_seed(args.seed)
+    data_path = os.path.join(args.data_dir, "train.csv")
+    print(data_path)
+
+    data = pd.read_csv(data_path)
+    print(data.head())
---
+++ utils.py
@@ -0,0 +1,10 @@
+import torch
+import random
+import numpy as np
+import pandas as pd
+
+
+def set_seed(seed=42):
+    np.random.seed(seed)
+    random.seed(seed)
+    return
