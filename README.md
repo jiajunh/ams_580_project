d0a45d57bbb153e7e3996fe338730ec5ffdc2f8c
jiajunh <jiajunh@umich.edu>
2025-03-12 20:50:35 -0400

    dive in features

--- README.md
+++ README.md
@@ -6,8 +6,20 @@
   ```
   pip install -r requirements.txt
   ```
-* 
+
+
 
 ## proprocesing
+
 * Three cols has " ?", not only "?", is missing values, so for every col, if the data type is str, first strip it.
 * ‘education’ column and ‘education-num’ column is identical, so actually we can just neglect one, I choose to drop ‘education’
+
+* ‘fnlwgt’ has a very biased distribution, maybe use log scale is better
+* ‘capital gain / capital loss’ has large range, so need to scale down, use log
+* native-country & race is highly biased, so maybe think to lower the
+* Can merge race to white & not white
+* Age to bin, each bin has similar percentage
+* education merge based on diploma
+* Marital merge into 3
+* Work class can merge ‘never-worked’ & ‘without-pay’
+* country merge to America & non-amerada
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import torch
 import random
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def set_seed(seed=42):
@@ -21,10 +22,10 @@ def preprocess(df):
         | fnlwgt
             - type: int
             - dinstinct_values: 18415
-        | education
+        | education (Identical to education-num, just drop it)
             - type: str (Categorical)
             - dinstinct_values: 16
-        | education-num (Identical to education)
+        | education-num 
             - type: int
             - dinstinct_values: 16
         | marital-status
@@ -58,12 +59,31 @@ def preprocess(df):
             - type: str (Binary)
             - dinstinct_values: 2
     '''
-    cols = df.columns.tolist()
     df = df.map(lambda x: x.strip() if isinstance(x, str) else x)
-    
     df = df.drop(columns="education")
-    # for col in cols:
-    #     print(col, type(df[col][0]), len(df[col].value_counts()))
+    df["income"] = np.where(df["income"]==">50K", 1, 0)
 
+
+    # EDA
+    df["fnlwgt"] = df["fnlwgt"].map(np.log)
+    # df["fnlwgt"] = df["fnlwgt"].map(lambda x: 1/x)
+    cols = df.columns.tolist()
+    fig, ax = plt.subplots(4,4, figsize=(12, 8))
+    plt.subplots_adjust(left=0.1, bottom=0.05, right=0.95, top=0.9, wspace=0.3, hspace=0.5)
+
+    for i, col in enumerate(cols):
+        r = i // 4
+        c = i % 4
+        if not isinstance(df[col][0], str):
+            df[col].plot(kind="hist", ax=ax[r, c])
+            ax[r, c].set_title(col)
+        else:
+            count = df[col].value_counts()
+            # print(type(count), count.iloc[1])
+            df[col].value_counts().plot.bar(ax=ax[r, c])
+            ax[r, c].set_title(col)
+    
+    plt.show()
+    
 
     return df
